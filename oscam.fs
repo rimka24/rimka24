cddb49624a3b9fe0d0ec555f5149766f9d2c0efa
rimka24 <103692381+rimka24@users.noreply.github.com>
2023-09-15 15:29:48 +0200

    Add files via upload

---
+++ oscam.fs
@@ -0,0 +1,29 @@
+###############################################
+# *******************************************
+# *          ..:: A U T H O R ::..          *
+# *             << Sisyphe >>               *
+# *******************************************
+
+https://cccam.net/free
+https://cccamprime.com/cccam48h.php
+https://cccamfrei.com/free/get.php
+https://cccamfrei.com/#cccam-free
+https://cccamas.com/free/get.php
+https://cccamas.com/#cccam-free
+https://cccam-premium.com/free-cccam/
+https://cccamiptv.club/fr/free-cccam/
+https://www.cccambird2.com/freecccam.php
+https://cccamia.com/free-cccam/
+https://cccamx.com/free-cccam
+https://cccamazon.com/free/get.php
+https://cccamazon.com/#cccam-free
+#https://pastebin.com/raw/4eb7McNm
+#https://pastebin.com/raw/nwU3m4hL
+#https://pastebin.com/raw/Sn3G2hfa
+#https://raw.githubusercontent.com/rimka24/rimka24/main/FreeCccam
+#https://raw.githubusercontent.com/rimka24/rimka24/main/CCcam.cfg
+#https://raw.githubusercontent.com/rimka24/rimka24/main/oscam
+#https://raw.githubusercontent.com/rimka24/rimka24/main/oscam.server
+#https://raw.githubusercontent.com/crazy-FROG1/SERVERS/main/CCcam.cfg
+#https://raw.githubusercontent.com/karimSATPRO/Cccampro/main/CCcam.cfg
+#https://raw.githubusercontent.com/karimSATPRO/Cccam/main/oscam.server
